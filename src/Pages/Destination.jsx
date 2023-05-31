ac18312518eb46d63ad86b46f0ed64059ea50423
nrkay <nkhairiyah018@gmail.com>
2023-05-31 11:46:47 +0700

    --

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -3,11 +3,23 @@ import mars from "../assets/mars.png"
 import moon from "../assets/moon.png"
 import europa from "../assets/europa.png"
 import titan from "../assets/titan.png"
+import { useState } from "react"
 
 
 function Destination() {
 
+    const [index, setIndex] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [descContent, setDescContent] = useState("See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take some history by visiting the Luna 2 and Apollo 11 landing sites.");
+    const [distanceContent, setDistanceContent] = useState();
+    const [time, setTime] = useState();
+    const [image, setImage] = useState();
+    const [title, setTitle] = useState("MOON");
+
+    console.log(image)
+
     const content = [{
+        "id": 1,
         "img": { moon },
         "title": "MOON",
         "desc": "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take some history by visiting the Luna 2 and Apollo 11 landing sites.",
@@ -15,6 +27,7 @@ function Destination() {
         "travel_time": "3 DAYS",
     },
     {
+        "id": 2,
         "img": { mars },
         "title": "MARS",
         "desc": "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!!",
@@ -22,6 +35,7 @@ function Destination() {
         "travel_time": "3 DAYS",
     },
     {
+        "id": 3,
         "img": { europa },
         "title": "EUROPA",
         "desc": "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxtion in your snug wintery cabin.",
@@ -29,6 +43,7 @@ function Destination() {
         "travel_time": "3 YEARS",
     },
     {
+        "id": 4,
         "img": { titan },
         "title": "TITAN",
         "desc": "The only moon known to have a dense atmosphere other than earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
@@ -58,33 +73,37 @@ function Destination() {
                 <div className="lg:mx-12 h-full grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 pt-20 md:pt-32">
                     <div className="grid justify-items-stretch">
                         <h1 className="text-white px-4 mt-6"><span className="font-bold">01</span> PICK YOUR DESTINATION</h1>
-                        <img className="w-3/5 lg:w-3/5 md:w-2/5 justify-self-center self-center lg:pt-8" src={planet} alt="" />
+                        <img className="w-3/5 lg:w-3/5 md:w-2/5 justify-self-center self-center lg:pt-8" src={image} alt="" />
                     </div>
                     <div className="px-11 lg:px-24 lg:pt-20">
                         <div className="destination-btn-desc flex justify-between">
-                            <button>MOON</button>
-                            <button>MARS</button>
-                            <button>EURPA</button>
-                            <button>TITAN</button>
+                            {content.map((item) => {
+                                return (
+                                    <>
+                                        <button key={item.id} className={`p-4 ${isActive ? 'active' : ''}`} onClick={(e) => {
+                                            e.preventDefault();
+                                            setIndex(item.id)
+                                            setDescContent(item.desc)
+                                            setDistanceContent(item.distance)
+                                            setImage(item.img)
+                                            setTitle(item.title)
+                                            setIsActive(!isActive)
+                                        }}>{item.title}</button>
+                                    </>
+                                )
+                            })}
                         </div>
-                        <div className="destination-desc-item mt-9">
-                            <div className="destination-items">
-                                <h1>MARS</h1>
-                                <p className="">Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!!</p>
-                            </div >
-                            <div className="destination-items">
-                                <h1>MOON</h1>
-                                <p className="">Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!!</p>
-                            </div>
-                            <div className="destination-items">
-                                <h1>AUROPE</h1>
-                                <p className="">Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!!</p>
-                            </div>
-                            <div className="destination-items">
-                                <h1>MARS</h1>
-                                <p className="">Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!!</p>
-                            </div>
+                        <div className="destination-desc-item mt-9 grid grid-rows-2">
+                            {/* {content.filter(item => item.id === index).map((item) => {
+                                return (
+                                    <>
+                                        <p>{item.desc}</p>
+                                    </>
+                                )
+                            })} */}
 
+                            <h1 className="md:mt-9">{title}</h1>
+                            <p className="md:mt-6">{descContent}</p>
                         </div>
                     </div>
                 </div>
